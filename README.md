4b02bf08b059bc4bd5b2bb10bd1f59bcd03749cc
dkvirus <3548310748@qq.com>
2020-10-30 12:55:03 +0800

    feat: add flask web server

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # PYNOVEL_REPTILE_PYTHON
 
-小说爬虫。
+小说爬虫服务。
+
+### 启动
+
+``` bash
+$ python3 index.py
+```
+
+访问：`http://localhost:5000/novels?keyword=黑色枷锁` 
+
+### API
+
+- GET /novels 根据关键字查询小说章节列表
+- GET /novel 查询指定小说基本信息
+- GET /chapters 查询章节列表
+- GET /chapter 查询章节内容
+
 
--- index.py
+++ index.py
@@ -1,33 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask, abort, request, jsonify
+
 from reptile_factory.factory import ReptileFactory
 
-# 根据关键词查询小说列表
-reptile_factory = ReptileFactory()
-novel_list = reptile_factory.get_novels_by_keyword('九灯和善')
-print(novel_list)
+app = Flask(__name__)
 
-# 查询小说详情
-# target_url = 'https://www.biquge5200.cc/75_75847/'
-# reptile_factory = ReptileFactory(target_url)
-# reptile = reptile_factory.get_reptile()
-# novel_intro = reptile.get_novel_intro()
-# print(novel_intro)
+# 根据关键字查询小说列表
+# @param keyword {string} 关键词
+@app.route('/novels', methods=['GET'])
+def get_novel_list():
+    if not request.args or 'keyword' not in request.args:
+        return jsonify({ 'code': '9999', 'message': '查询关键字不能为空', 'data': None })
+    
+    reptile_factory = ReptileFactory()
+    novel_list = reptile_factory.get_novels_by_keyword(request.args['keyword'])
+    return jsonify({ 'code': '0000', 'message': '查询小说列表成功', 'data': novel_list })
+
+# 查询小说详情，包括作者名、书名、封面url地址、更新时间、简介、第一章url地址、分类名称
+# @param url {string} 小说url地址
+@app.route('/novel', methods=['GET'])
+def get_novel():
+    if not request.args or 'url' not in request.args:
+        return jsonify({ 'code': '9999', 'message': '小说url地址不能为空', 'data': None })
+
+    reptile_factory = ReptileFactory(request.args['url'])
+    reptile = reptile_factory.get_reptile()
+    novel = reptile.get_novel_intro()
+    return jsonify({ 'code': '0000', 'message': '查询小说介绍成功', 'data': novel })
 
 # 查询小说章节列表
-# target_url = 'https://www.biquge5200.cc/75_75847/'
-# reptile_factory = ReptileFactory(target_url)
-# reptile = reptile_factory.get_reptile()
-# chapter_list = reptile.get_chapter_list()
-# print(len(chapter_list))
-# print(chapter_list[:10])
+# @param url {string} 小说url地址
+@app.route('/chapters', methods=['GET'])
+def get_chapter_list():
+    if not request.args or 'url' not in request.args:
+        return jsonify({ 'code': '9999', 'message': '小说url地址不能为空', 'data': None })
 
-# 查询小说章节内容
-# target_url = 'https://www.biquge5200.cc/75_75847/146728646.html'
-# reptile_factory = ReptileFactory(target_url)
-# reptile = reptile_factory.get_reptile()
-# chapter_content = reptile.get_chapter_content()
-# print(chapter_content)
+    reptile_factory = ReptileFactory(request.args['url'])
+    reptile = reptile_factory.get_reptile()
+    chapter_list = reptile.get_chapter_list()
+    return jsonify({ 'code': '0000', 'message': '查询小说章节列表成功', 'data': chapter_list })
 
-# 切换源
-# target_url = 'https://www.biquge5200.cc/'
-# reptile_factory = ReptileFactory()
-# reptile_factory.get_novel_by_author_and_bookname(author_name='月半云遮', book_name='余年不庆', origin=target_url)
+# 查询小说章节内容，包括章节标题、章节内容、上一章节url地址、下一章节url地址
+# @param url {string} 章节url地址
+@app.route('/chapter', methods=['GET'])
+def get_chapter():
+    if not request.args or 'url' not in request.args:
+        return jsonify({ 'code': '9999', 'message': '小说章节url地址不能为空', 'data': None })
+
+    reptile_factory = ReptileFactory(request.args['url'])
+    reptile = reptile_factory.get_reptile()
+    chapter = reptile.get_chapter_content()
+    return jsonify({ 'code': '0000', 'message': '查询小说章节内容成功', 'data': chapter })
+
+if __name__ == "__main__":
+    app.run(debug=True)
